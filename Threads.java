8e71e41983ee7f9dce8bdd3445a8f6bb53ccc063
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-02-14 10:13:18 +0530

    lazy threads

--- Threads.java
+++ Threads.java
@@ -1,72 +1,110 @@
 import java.awt.*;
-import java.event.*;
-import javax.swing;
+import java.awt.event.*;
+import javax.swing.*;
 import java.io.*;
-import java.util.*;
 
-public class Threads implements ActionListener{
-  Threads(){
-    JFrame frm = new JFrame("hello krabby");
-    frm.setSize(800,800);
-    frm.setLayout(null);
+public class Threads implements ActionListener {
+    JFrame frm;
+    JButton bn;
+    JTextField texts[];
+    JTextField numbers[];
+    JButton bg;
+    JPanel panel;
+    JTextField number;
 
-    JTextField number = new TextField("Enter how many words u want to write ");
-    JButton bn = new JButton("Generate");
-    bn.addActionListener(this);
-    frm.setVisible(true);
-  }
+    Threads() {
+        frm = new JFrame("hello krabby");
+        frm.setSize(800, 800);
+        frm.setLayout(new FlowLayout());
 
-  public void actionPerfomed(ActionEvent ae){
-      int n = (int) number.getText();
-    if (ae.getSource == bn){
-      JTextField texts[] = new JTextField[10];
-      JTextField numbers[] = new JTextField[10];
-      JPanel panel[] = new JPanel[10];
-      for(int i = 0; i<n; i++){
-        text[i].setText("Add your word");
-        numbers[i].setText("Add you times");
-        panel[i].add(text[i]);
-        panel[i].add(numbers[i]);
-        frm.add(panel[i]);
-      }
-      public JButton bg = JButton("Write!");
-      bg.addActionListener(this);
-      frm.add(bg);
-    }
-   
-  if (ae.getSource() == bg){
-    for(int i = 0;i<n; i++){
-      String s = text[i].getText();
-      int n = (int)numbers.getText();
-      WordPrinter wp = new WordPrinter(s,n);
+        number = new JTextField("Enter how many words u want to write ", 20);
+        bn = new JButton("Generate");
+        bn.addActionListener(this);
+        frm.add(number);
+        frm.add(bn);
+
+        frm.setVisible(true);
     }
 
-  }  
+    public void actionPerformed(ActionEvent ae) {
+        int n = Integer.parseInt(number.getText());
+        if (ae.getSource() == bn) {
+            panel = new JPanel();
+            panel.setLayout(new GridLayout(n, 2));
+
+            texts = new JTextField[n];
+            numbers = new JTextField[n];
+
+            for (int i = 0; i < n; i++) {
+                texts[i] = new JTextField("Add your word", 15);
+                numbers[i] = new JTextField("Add your times", 10);
+                panel.add(texts[i]);
+                panel.add(numbers[i]);
+            }
+
+            bg = new JButton("Write!");
+            bg.addActionListener(this);
+            frm.add(panel);
+            frm.add(bg);
+            frm.revalidate();
+            frm.repaint();
+        } else if (ae.getSource() == bg) {
+            for (int i = 0; i < n; i++) {
+                String s = texts[i].getText();
+                int times = Integer.parseInt(numbers[i].getText());
+                WordPrinter wp = new WordPrinter(s, times);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Threads();
+    }
+}
+
+class WordPrinter implements Runnable {
+    String text;
+    int n;
+
+    WordPrinter(String textIn, int nIn) {
+        text = textIn;
+        n = nIn;
+        Thread t = new Thread(this);
+          System.out.println("" + t.getName());
+        t.start();
+        try{
+          Thread main = Thread.currentThread();
+          main.sleep(600);
+        }catch(Exception e){
+          e.printStackTrace();
+        }
+    }
+
+    synchronized public void appendToFile(String filename, String content) {
+        try {
+            File file = new File(filename);
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file, true);
+            writer.append(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
-  }
-}
+    synchronized public void output(){
+     for (int i = 0; i < n; i++) {
+            System.out.println(this.text + "\n");
+            appendToFile("threads.txt", "" + this.              text + "\n");
 
-class WordPrinter implements Runnable{
-  WordPrinter(String textIn,int nIn){
-    String text = textIn;
-    int n = nIn;
-    Thread t = new Thread(this);
-    t.start();
-  }
+            
+      }
+    }
+     public void run() {
+       synchronized(this){
+          output(); 
+       }
 
-  public void appendToFile(String filename,String content){
-    File file = new File(filename);
-    file.createNewFile();
-    FileWriter writer = new FileWriter(file,true);
-    writer.append(content);
-    writer.close();
-  }
+        }
+     }
 
-  synchronized public void run(){
-   for(int i = 0; i<this.n; i++){
-     System.out.println(this.text + "\n");
-    appendToFile("threads.x",""+this.text+"\n");
-   } 
-  }
-
-}
